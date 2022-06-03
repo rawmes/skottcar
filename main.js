5d56c56c75d229422571ae03929529ac9b1e5d8b
Bill Dallas <11507104+BillGR17@users.noreply.github.com>
2022-06-03 11:36:58 +0300

    sanic

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ canvas.width = 600;
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width/2,canvas.width*0.9)
-const car = new Car(road.getLaneCenter(1),100,30,50,"KEYS",3);
+const car = new Car(road.getLaneCenter(1),100,30,50,"KEYS",9);
 const traffic = [
 	new Car(road.getLaneCenter(0),-100,30,50,"DUMMY",2)
 ];
